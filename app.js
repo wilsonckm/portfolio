88444a03b8152b1c72a6eab15fb39bdc171f2386
wilsonckm <wilsonckmchan@gmail.com>
2023-06-06 13:33:21 -0400

    Commit 4: Contact form completed. Adjusting placement. Trialing with html form/method/action.

--- app.js
+++ app.js
@@ -14,37 +14,60 @@ const observer = new IntersectionObserver ((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+const data = [
+    {firstName:"Wilson", lastName: "Chan", email: "wilson@gmail.com", message: "Hope this works!"},
+];
+
+const addData = (fName, lName, emailParam, messageParam) => {
+    data.push({firstName: fName, lastName: lName, email: emailParam, message: messageParam});
+}
+
 $(() => {
-    let slideCounter =1;
-    showSlides(slideIndex);
+    $('#submit').on('click', () => {
+        const newFirstName = $('#firstName').val();
+        const newLastName = $('#lastName').val();
+        const newEmail = $('#email').val();
+        const message = $('#message').val();
+        //console.log(newFirstName, newLastName, newEmail, message);
+        addData(newFirstName, newLastName, newEmail, message);
+        console.log(data)
+        //$('section').addClass('hidden');
+        $('div.footer').append('Thank you! Talk to you soon!')
+    })
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        const slides=document.getElementsByClassName('carousel');
-        const dots = document.getElementsByClassName('dot');
-        if (n > slides.length) {slideIndex=1}
-        if (n <1) {slideIndex = slide.length}
-        for (i=0; i<slides.length; i++) {
-            slides[i].getElementsByClassName.display = 'none';
-        }
-        for (i=0; i<slide.length; i++) {
-            slides[i].getElementsByClassName.display = 'none';
-        }
-        for (i=0; i<dots.length; i++) {
-            dots[i].clasName = dots[i].className.replace('active', "");
-        }
-        slides[slideIndex-1].style.display='block';
-        dots[slideIndex-1].className += "active";
-    }
-
+    
 });
 
 
 
+
+
+//carousel??
+    // let slideCounter =1;
+    // showSlides(slideIndex);
+
+    // function plusSlides(n) {
+    //     showSlides(slideIndex += n);
+    // }
+
+    // function currentSlide(n) {
+    //     showSlides(slideIndex = n);
+    // }
+
+    // function showSlides(n) {
+    //     const slides=document.getElementsByClassName('carousel');
+    //     const dots = document.getElementsByClassName('dot');
+    //     if (n > slides.length) {slideIndex=1}
+    //     if (n <1) {slideIndex = slide.length}
+    //     for (i=0; i<slides.length; i++) {
+    //         slides[i].getElementsByClassName.display = 'none';
+    //     }
+    //     for (i=0; i<slide.length; i++) {
+    //         slides[i].getElementsByClassName.display = 'none';
+    //     }
+    //     for (i=0; i<dots.length; i++) {
+    //         dots[i].clasName = dots[i].className.replace('active', "");
+    //     }
+    //     slides[slideIndex-1].style.display='block';
+    //     dots[slideIndex-1].className += "active";
+    // }
