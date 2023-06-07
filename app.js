bf5f3ed39c8b71226a0994725256c8862eaa0b86
wilsonckm <wilsonckmchan@gmail.com>
2023-06-07 13:13:36 -0400

    Commit 5: Got carousel working! Will update sizing and photos

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
-const observer = new IntersectionObserver ((entries) => {
-    //Built in browser API
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-        //example of a callback function: a function that is appsed as an argument to another function.
-    });
-})
+// const observer = new IntersectionObserver ((entries) => {
+//     //Built in browser API
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//         //example of a callback function: a function that is appsed as an argument to another function.
+//     });
+// })
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
 
 const data = [
     {firstName:"Wilson", lastName: "Chan", email: "wilson@gmail.com", message: "Hope this works!"},
@@ -22,7 +22,33 @@ const addData = (fName, lName, emailParam, messageParam) => {
     data.push({firstName: fName, lastName: lName, email: emailParam, message: messageParam});
 }
 
+// indexImage = (urlArr) => {
+//     for (let i=0; i<urlArr.length; i++){
+//         if (i > urlArr.length) {
+//             i === 0
+//             return urlArr[i];
+//         } else return urlArr[i];
+//     }
+// }
+
+let indexImage = 0;
+const imageArr = [
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1604593042837-6GY50U84DMA06VIVW4BP/WCKM-0564.jpg?format=750w', 
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1604593064545-LA26MFZJ52EZ76EQ1P6W/WCKM-0560.jpg' ,
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1555388710371-3H3HHF31X20Q6LN8KDRX/WCKM-4-2.jpg?format=750w' ,
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1555388779811-2IPB8FL2DVYOXQH9HGAG/WCKM-4.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1556597199650-BMJVREXZ7TZY2OT3HENO/WCKM-2584.jpg?format=2500w' ,
+    'https://images.squarespace-cdn.com/content/v1/570084ed62cd94d3a2ac2196/1555388670850-335M3F06Z8460G9PZZCQ/WCKM-1-2.jpg?format=1500w' 
+]
+
+const showImage = () => {
+const $imageDiv = $('.imgSlide');
+  $imageDiv.empty();
+  $imageDiv.append(`<img src="${imageArr[indexImage]}">`);
+};
+
 $(() => {
+    //Contact page: submit button
     $('#submit').on('click', () => {
         const newFirstName = $('#firstName').val();
         const newLastName = $('#lastName').val();
@@ -32,10 +58,63 @@ $(() => {
         addData(newFirstName, newLastName, newEmail, message);
         console.log(data)
         //$('section').addClass('hidden');
-        $('div.footer').append('Thank you! Talk to you soon!')
+        $('').append('Thank you! Talk to you soon!')
+    })
+    
+    //carousel:
+    //next button:
+
+    const $imageDiv = $('<div>').css('position', 'relative');
+    
+    // const $nextButton = $('<div>').addClass('carousel-button next').text('>');
+    // $('.imgSlide').append($nextButton);
+    const $nextButton = $(".carousel-button.next");
+    $nextButton.css('cursor', 'pointer');
+    
+
+    $nextButton.on('click', () => {
+        console.log('next!')
+        indexImage = (indexImage + 1) % imageArr.length;
+        showImage()
+
+        // if (indexImage >= imageArr.length-1) {
+        //     indexImage = 0;
+        //     console.log(indexImage);
+        // } else {
+        //     indexImage += 1;
+            
+        //     console.log(indexImage);
+        // }
+        // $imageDiv.append(`<img id = "Wilson's Image" src = ${imageArr[indexImage]} />`);
+        // $('.image-container').append($imageDiv);
+        
+    })
+    //previous button
+    // const $prevButton = $('<div>').addClass('carousel-button prev').text('<');
+    // $('.imgSlide').prepend($prevButton);
+    const $prevButton = $(".carousel-button.prev");
+    $prevButton.css('cursor', 'pointer');
+    
+    $prevButton.on('click', () => { 
+       console.log('prev!')
+       
+       indexImage = (indexImage - 1 + imageArr.length) % imageArr.length;
+       showImage()
+
+        // if (indexImage < 1 ) {
+        //     indexImage = imageArr.length-1;
+        //     console.log(indexImage);
+        // } else {
+        //     indexImage -= 1;
+            
+        //     console.log(indexImage);
+        // }
+        // $imageDiv.append(`<img id = "Wilson's Image" src = ${imageArr[indexImage]} />`);
+        // $('.image-container').append($imageDiv);
+    
     })
 
-    
+    showImage();
 });
 
 
